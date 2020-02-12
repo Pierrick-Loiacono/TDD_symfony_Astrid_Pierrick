77590805aa9cb275f4c46b2e7611845c32c322ee
Pierrick <pierrick.loiacono@arrobe.fr>
2020-02-12 14:01:16 +0100

    Test Trajet

--- tests/Entity/TrajetTest.php
+++ tests/Entity/TrajetTest.php
@@ -2,7 +2,10 @@
 namespace App\Tests\Entity;
 
 use App\Entity\Trajet;
+use App\Entity\Lieu;
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 
 class TrajetTest extends TestCase {
@@ -17,6 +20,45 @@ class TrajetTest extends TestCase {
     public function testTrajet(){
         $this->assertInstanceOf(Trajet::class, $this->trajet);
         $this->assertNull($this->trajet->getId());
+    }
+
+    public function testTrajetLieuDepart(){
+        $lieu = new Lieu();
+        $lieu->setNom("Paris");
+        $this->trajet->setLieuDepart($lieu);
+        $this->assertEquals($lieu, $this->trajet->getLieuDepart());
+
+    }
+
+    public function testTrajetLieuArrive(){
+        $lieu = new Lieu();
+        $lieu->setNom("Paris");
+        $this->trajet->setLieuArrive($lieu);
+        $this->assertEquals($lieu, $this->trajet->getLieuArrive());
+    }
+
+    public function testTrajetDateHeureDepart() {
+        $date = new \DateTime();
+        $this->trajet->setDate($date);
+        $this->assertEquals($date, $this->trajet->getDate());
+
+    }
+
+    public function testTrajetConducteur(){
+        $user = new User();
+        $this->trajet->setConducteur($user);
+        $user->addTrajet($this->trajet);
+        $this->assertEquals($user, $this->trajet->getConducteur());
+        $this->assertContains($this->trajet, $user->getTrajets());
+
+    }
+
+    public function testTrajetPassager(){
+        $user = new User();
+        $this->trajet->addPassager($user);
+        $user->addTrajet($this->trajet);
+        $this->assertContains($user, $this->trajet->getPassagers());
+        $this->assertContains($this->trajet, $user->getTrajets());
     }
 
 }
