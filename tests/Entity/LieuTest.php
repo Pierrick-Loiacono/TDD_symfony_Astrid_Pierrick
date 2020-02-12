a4ee7e63a3c9b3566f46d0d07b211188b6d11297
Pierrick <pierrick.loiacono@arrobe.fr>
2020-02-12 11:03:14 +0100

    test lieu + test lieu nom

---
+++ tests/Entity/LieuTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+
+class LieuTest extends TestCase {
+
+    protected $lieu;
+
+    public function setUp() :void
+    {
+        $this->lieu = new Lieu();
+    }
+
+    public function testLieu(){
+        $this->assertInstanceOf(Lieu::class, $this->lieu);
+        $this->assertNull($this->lieu->getId());
+    }
+
+    public function testLieuNom(){
+        $this->lieu->setNom("France");
+        $this->assertEquals("France", $this->lieu->getNom());
+    }
+
+}
