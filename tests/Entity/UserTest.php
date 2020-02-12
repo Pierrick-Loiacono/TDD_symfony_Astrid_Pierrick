31117a207ac7ac4eadd0f6b515f9cf7fe5df65a9
Pierrick <pierrick.loiacono@arrobe.fr>
2020-02-12 10:17:16 +0100

    first commit

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+use App\Entity\User;
+
+
+class UserTest extends TestCase {
+
+    public function testNewUser(){
+        $this->assertInstanceOf(User::class, new User());
+    }
+
+
+}
