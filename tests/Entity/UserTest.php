22ecbe888ddf87f436f94a407213bd4ed316ae16
Pierrick <pierrick.loiacono@arrobe.fr>
2020-02-12 10:35:20 +0100

    Test prenom User

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -7,14 +7,25 @@ use App\Entity\User;
 
 class UserTest extends TestCase {
 
+    protected $user;
+
+    public function setUp() :void
+    {
+        $this->user = new User();
+    }
+
     public function testNewUser(){
         $this->assertInstanceOf(User::class, new User());
     }
 
     public function testUserNom(){
-        $user = new User();
-        $user->setNom("Patrick");
-        $this->assertEquals("Patrick", $user->getNom());
+        $this->user->setNom("Patrick");
+        $this->assertEquals("Patrick", $this->user->getNom());
+    }
+
+    public function testUserPrenom(){
+        $this->user->setPrenom("Paulien");
+        $this->assertEquals("Paulien", $this->user->getPrenom());
     }
 
 
