cf39d1c0d8465d177661cceb3fab8352b4707e08
Pierrick <pierrick.loiacono@arrobe.fr>
2020-03-03 14:29:22 +0100

    Crud de Trajet

---
+++ src/Form/TrajetType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trajet;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrajetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('places')
+            ->add('lieu_depart')
+            ->add('lieu_arrive')
+            ->add('conducteur')
+            ->add('passagers')
+            ->add('lieu')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trajet::class,
+        ]);
+    }
+}
