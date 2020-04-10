8a837f810d2eee8272f637e24797d9c5041da2af
Pierrick Loiacono <pierrick38800@gmail.com>
2020-04-10 15:00:54 +0200

    Ajout d'une date initial dans le formulaire de trajet

--- src/Form/TrajetType.php
+++ src/Form/TrajetType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Lieu;
 use App\Entity\Trajet;
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,7 +16,9 @@ class TrajetType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
+            ->add('date', DateTimeType::class, [
+                'data' => new \Datetime()
+            ])
             ->add('lieu_depart', EntityType::class, [
                 'class' => Lieu::class,
                 'choice_label' => 'nom',
