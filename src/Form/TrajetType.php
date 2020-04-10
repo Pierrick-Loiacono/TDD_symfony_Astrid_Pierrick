98b296d999f95c833e24e8c94246197c6c89ce16
Pierrick Loiacono <pierrick38800@gmail.com>
2020-04-10 13:59:37 +0200

    Complétion du Form pour la création d'un trajet (Ajout des champs nécessaire à la création d'un trajet)

--- src/Form/TrajetType.php
+++ src/Form/TrajetType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Lieu;
 use App\Entity\Trajet;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +16,19 @@ class TrajetType extends AbstractType
     {
         $builder
             ->add('date')
-        ;
+            ->add('lieu_depart', EntityType::class, [
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('lieu_arrive', EntityType::class, [
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('places')
+            ->add('conducteur', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'username',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
