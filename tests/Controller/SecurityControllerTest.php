347b4eeec0461d33f1a3b1f716070ff2e9b6c82c
Pierrick Loiacono <pierrick38800@gmail.com>
2020-04-10 16:04:57 +0200

    Test du formulaire de connexion avec redirection sur la liste des trajets

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -6,17 +6,46 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SecurityControllerTest extends WebTestCase {
 
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
     public function testLogin() {
-        $client = static::createClient();
-        $client->request('GET', '/login');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->client->request('GET', '/login');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
     public function testLogout() {
-        $client = static::createClient();
-        $client->followRedirects();
-        $client->request('GET', '/logout');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->client->followRedirects();
+        $this->client->request('GET', '/logout');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testLoginForm(){
+        //Récupération du contenu de la page
+        $crawler = $this->client->request('GET', '/login');
+
+        //Récupération du formulaire grâce au texte que contient le bouton associé au formulaire
+        $form = $crawler->selectButton('Sign in')->form();
+
+        // On set les valezurs du formualire
+        $form->setValues([
+            'username' => 'username',
+            'password' => 'pa$$word',
+        ]);
+        // Soumission du formulaire
+        $crawler = $this->client->submit($form);
+        // On vérifie qu'on a une redirection
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+        // On récupére la redirection ainsi que le contenu de la page sur laquelle on est redirigé
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode()); // On verifie qu'on a bien un code 200
+        $this->assertContains('/trajet/liste', $crawler->getUri()); // On verifie qu'on a bien un code 200
+
     }
 
 }
