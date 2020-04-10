cc91f85c3599827d817d909fe85dd763c3a706aa
Pierrick Loiacono <pierrick38800@gmail.com>
2020-04-10 13:58:48 +0200

    Test de soumission du formulaire de création de trajet

--- tests/Controller/TrajetControllerTest.php
+++ tests/Controller/TrajetControllerTest.php
@@ -2,7 +2,10 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Lieu;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 
 class TrajetControllerTest extends WebTestCase
@@ -40,9 +43,49 @@ class TrajetControllerTest extends WebTestCase
 
     }
 
+    public function testCreateTrajet()
+    {
+        $username = 'username';
+        $password = 'pa$$word';
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => $username,
+            'PHP_AUTH_PW' => $password,
+        ]);
+
+        // Récupération de l'utilisateur connecté dans la base
+        $kernel = self::bootKernel();
+        $entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+        $user = $entityManager
+            ->getRepository(User::class)
+            ->findOneBy(['username' => $username]);
+        $lieuDepart = $entityManager
+            ->getRepository(Lieu::class)
+            ->findOneBy(['nom' => 'France']);
+        $lieuArrive = $entityManager
+            ->getRepository(Lieu::class)
+            ->findOneBy(['nom' => 'Portugal']);
+
+        $this->assertEquals('username', $user->getUsername());
+        $crawler = $client->request('GET', '/trajet/new');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $buttonCrawlerNode = $crawler->selectButton('Save');
+
+        $form = $buttonCrawlerNode->form([
+            'trajet[lieu_depart]' => $lieuDepart->getId(),
+            'trajet[lieu_arrive]' => $lieuArrive->getId(),
+            'trajet[places]' => 4,
+            'trajet[conducteur]' => $user->getId(),
+        ]);
+        $crawler = $client->submit($form);
+
+
+    }
+
     // scénario test crétion de trajet
-    // poster le formulaire de création de trajets -- Fait
-    // aller sur /trajet/liste -- Fait
+    // poster le formulaire de création de trajets
+    // aller sur /trajet/liste
     // vérifier que le trajet exsite même date
     // vérifier que le conducteur est l'utilisateur courant
 
